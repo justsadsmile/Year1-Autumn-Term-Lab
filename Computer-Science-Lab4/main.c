f9aa7a14c7c025ee22f6312fb53c3dd77497533c
justsadsmile <giveme5starbaton@gmail.com>
2025-11-30 03:56:36 +0400

    add second task code

--- Computer-Science-Lab4/main.c
+++ Computer-Science-Lab4/main.c
@@ -1,8 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include "config.h"
+#include <math.h>
 
-
+#define ROUND_TO_DECIMAL(value, decimals) (round((value) * pow(10, decimals)) / pow(10, decimals))
 
 int Function(int n) {
 	if (n < 2) {
@@ -12,7 +13,7 @@ int Function(int n) {
 	if (n % 2 == 0) {
 		return Function(n / 2) + 1;
 	}
-	else{
+	else {
 		return Function(n - 1) + n;
 	}
 }
@@ -46,14 +47,52 @@ int First2(int x) {
 	return count;
 }
 
-int Second() {
+double DoubleFactorial(int n) {
+	if (n < 0) {
+		return 0;
+	}
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	return DoubleFactorial(n - 2) * n;
+}//with int only 0!! to 19!!
 
+double SumOriginalFunction(int N, double x) {
+	double sum = 0;
+
+	for (int n = 1; n <= N; n++) {
+		double numerator = 2 * n * pow(x, 2 * n + 1);
+		double denominator = DoubleFactorial(2 * n + 1);
+		double term = numerator / denominator;
+		sum += term;
+	}
+	return sum;
+}
+
+double SumRecurrentFunction(int N, double x) {
+	if (N < 1) return 0;
+
+	double a_n = (2 * pow(x, 3)) / 3.0;
+	double sum = a_n;
+
+	for (int n = 1; n < N; n++) {
+		a_n = a_n * (pow(x, 2) * (n + 1)) / (n * (2 * n + 3));
+		sum += a_n;
+	}
+	return sum;
 }
 
 int GetInt(char text[]) {
 	printf("%s", text);
 	int x;
 	scanf("%d", &x);
+	return x;
+}
+
+double GetDouble(char text[]) {
+	printf("%s", text);
+	double x;
+	scanf("%lf", &x);
 	return x;
 }
 
@@ -73,6 +112,24 @@ int main() {
 	//2
 	printf("\n===== 2 =====\n");
 
+	int secondN = GetInt("Enter N: ");
+	double secondX = GetDouble("Enter x: ");
+
+	double sumOriginal = SumOriginalFunction(secondN, secondX);
+	double sumRecurrent = SumRecurrentFunction(secondN, secondX);
+
+	sumOriginal = ROUND_TO_DECIMAL(sumOriginal, 6);
+	sumRecurrent = ROUND_TO_DECIMAL(sumRecurrent, 6);
+
+	printf("Original:  %f\n", sumOriginal);
+	printf("Recurrent: %f\n", sumRecurrent);
+
+	if (sumOriginal == sumRecurrent) {
+		printf("Both sums are equal");
+	}
+	else {
+		printf("Both sums are NOT equal");
+	}
 
 	return 0;
 }
